db56f60c68c75ce6192de40e22c135b95ce95ad9
xwarhamx <aleksandrmar87@gmail.com>
2026-06-13 21:27:52 +0300

    feat: update to version 2.0

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>🚀 Мой первый DevOps проект!</h1><p>Версия 1.0</p>"
+    return "<h1>🚀 Мой первый DevOps проект!</h1><p>Версия 2.0</p>"
 
 
 @app.route("/health")
